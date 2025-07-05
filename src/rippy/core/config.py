1b69973e36ff9fdedc3029da5c2c99f076a8064b
TylerDurham <TylerDurham@users.noreply.github.com>
2025-07-04 20:10:06 -0500

    code clean/comments.

--- src/rippy/core/config.py
+++ src/rippy/core/config.py
@@ -3,23 +3,37 @@ import tomlkit as tk
 from pathlib import Path
 import os
 
-APP_NAME = "rippy"
-HOME_DIR = os.path.expanduser("~")
-CONFIG_DIR = os.path.join(HOME_DIR, f".config/{APP_NAME}")
-CONFIG_FILE = f"{APP_NAME}.toml"
-CONFIG_FILE_PATH = os.path.join(CONFIG_DIR, CONFIG_FILE)
-RIP_DIR = os.path.join(HOME_DIR, APP_NAME.capitalize()) 
-IMPORT_DIR = os.path.join(RIP_DIR, "@import")
-MOVIES_DIR = os.path.join(RIP_DIR, "movies")
-TV_SHOWS_DIR = os.path.join(RIP_DIR, "tv-shows")
+APP_NAME = "rippy" # The name of the app.
+HOME_DIR = os.path.expanduser("~") # Path to the user's home directory.'
+CONFIG_DIR = os.path.join(HOME_DIR, f".config/{APP_NAME}") # Path to the config directory.
+CONFIG_FILE = f"{APP_NAME}.toml" # The name of the config file.
+CONFIG_FILE_PATH = os.path.join(CONFIG_DIR, CONFIG_FILE) # The full path to the config file.
+RIP_DIR = os.path.join(HOME_DIR, APP_NAME.capitalize()) # The root app directory.
+IMPORT_DIR = os.path.join(RIP_DIR, "@import") # The path to where MKVs are imported.
+MOVIES_DIR = os.path.join(RIP_DIR, "movies") # The path where movies are converted. presumably by Handbrake.
+TV_SHOWS_DIR = os.path.join(RIP_DIR, "tv-shows") # The path where TV shows are converted, presumably by Handbrake
 
 @dc.dataclass
 class RippyConfigCore:
+    """
+    The root of the config data.
+    """
     api_key: str = ""
     rip_dir: str = RIP_DIR
 
 @dc.dataclass 
 class RippyConfig:
+    """
+    Class the holds config data in memory. Will be written to disk as TOML.
+
+    {
+        core {
+            api_key = "string"
+            rip_dir = "string"
+        }
+    }
+
+    """
     core: RippyConfigCore = dc.field(default_factory=RippyConfigCore)
 
     def has_api_key(self) -> bool:
@@ -37,30 +51,43 @@ def _from_dict(cls, data: dict):
     })
 
 
-def ensure_path(pathtc: str) -> bool:
+def ensure_path(path_to_check: str) -> bool:
     """
     Ensures the specified path exists. If it doesn't, the path is created.
 
     Args:
-        pathtc (str): The path to check.
+        path_to_check (str): The path to check.
 
     Returns:
         bool: True if the path was created, False otherwise.
     """
-    if not os.path.exists(pathtc):
-        os.makedirs(pathtc)
+    if not os.path.exists(path_to_check):
+        os.makedirs(path_to_check)
         return True
     else:
         return False
 
 def read_file(path_to_config_file: str = CONFIG_FILE_PATH) -> RippyConfig:
+    """
+    Reads the configurtion from the configuration directory.
 
-    s = ""
+    Args:
+        path_to_config_file (str): Opetional. The file to read.
+
+    Returns:
+        RippyConfig: A configuration object deserialized from the configuration file.
+    """
+
+    # Read file into string
+    toml_str = ""
     with open(path_to_config_file, "r") as f:
-        s = f.read()
-    toml = tk.loads(s)
-    data = _from_dict(RippyConfig, toml)
-    return data
+        toml_str = f.read()
+
+    # Load toml dictionary
+    toml = tk.loads(toml_str)
+
+    # Convert from toml dictionary to dataclass
+    return _from_dict(RippyConfig, toml)
 
 def write_file(config: RippyConfig, overwrite: bool = False, path_to_config_file: str = CONFIG_FILE_PATH):
     """
@@ -71,17 +98,23 @@ def write_file(config: RippyConfig, overwrite: bool = False, path_to_config_file
         overwrite (bool): If the config file exists, overwrite. 
 
     """
-    exists = os.path.exists(path_to_config_file)
-    can_write = (not exists) or (exists and overwrite)
 
-    if can_write:
+    # Does an existing config exist?
+    exists = os.path.exists(path_to_config_file)
+
+    # Do not write if exsists UNLESS overwrite is true
+    allow_write = (not exists) or (exists and overwrite)
+
+    if allow_write:
+        # Make sure the parent directory exists
         parent = f"{Path(path_to_config_file).parent}"
         ensure_path(parent)
 
-        toml = dc.asdict(config)
+        # Convert dataclass to dictionary for serialization
+        dict = dc.asdict(config)
 
         with open(path_to_config_file, "w") as f:
-            f.write(tk.dumps(toml))
+            f.write(tk.dumps(dict))
 
     else:
         raise FileExistsError(path_to_config_file)
