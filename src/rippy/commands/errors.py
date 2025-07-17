04be538364bdcc09d9d971de6f716cc79c35052d
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-07-16 22:29:32 -0500

    refactor(init): replace ValueError with custom exceptions
    - Moved error messages into dedicated exceptions in `rippy.commands.errors`
    - Replaced inline ValueError instances with `E_INIT_TITLE_WITH_READ` and `E_INIT_SEARCH_NO_READ_OR_TITLE`
    - Improves clarity, reusability, and testability of error handling
    	new file:   src/rippy/commands/errors.py

---
+++ src/rippy/commands/errors.py
@@ -0,0 +1,8 @@
+
+
+def E_INIT_TITLE_WITH_READ():
+    return ValueError("Invalid input: title should not be used with read.")
+
+def E_INIT_SEARCH_NO_READ_OR_TITLE():
+    return ValueError("Invalid input: `--search` should be used with either `title` or `--read`.")
+
