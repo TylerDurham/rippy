bd93fb349a167b4efc3d06b69bef11adbd6dd8a0
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-07-16 21:28:31 -0500

    refactor(init): extract input validation logic into helper function
    - Moved argument validation from `init()` to new `check_inputs()` function
    - Improves clarity and testability of the `init` command logic
    	modified:   src/rippy/commands/init.py

--- src/rippy/commands/init.py
+++ src/rippy/commands/init.py
@@ -22,17 +22,22 @@ def init(
     """
     Initializes a new {app_name} directory for a movie or TV show.
     """
+    
+    # Sanity check inputs
+    check_inputs(title, read, search)
+
+    print(f"type: {type}")
+    print(f"title: {title}")
+    print(f"read: {read}")
+    print(f"search: {search}")
+
+def check_inputs(title: str, read: bool, search: bool):
 
     if len(title) > 0 and read:
         raise ValueError("Invalid input: title should not be used with read.")
 
     if len(title) == 0 and search and not read:
         raise ValueError("Invalid input: `--search` should be used with either `title` or `--read`.")
-
-    print(f"type: {type}")
-    print(f"title: {title}")
-    print(f"read: {read}")
-    print(f"search: {search}")
 
 
 if __name__ == "__main__":
