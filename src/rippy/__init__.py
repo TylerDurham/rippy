510f77e7d0ef454fa69830f41a435476ed17c2a3
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-07-16 21:14:57 -0500

    Initial commit.

---
+++ src/rippy/__init__.py
@@ -0,0 +1,7 @@
+from rippy.main import app
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    main()
---
+++ src/rippy/main.py
@@ -0,0 +1,8 @@
+import typer as t 
+from rippy.commands import config, init
+
+app = t.Typer()
+
+app.add_typer(config.app, name="config")
+app.add_typer(init.app)
+
